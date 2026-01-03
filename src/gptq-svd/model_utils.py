a107bbcb65dfa3a467e3a9f127af0daf336721aa
David Tweedle <david.aig.tweedle@gmail.com>
2026-01-03 16:59:48 -0500

    back to flash attention

--- src/gptq-svd/model_utils.py
+++ src/gptq-svd/model_utils.py
@@ -9,7 +9,7 @@ def get_model(model_id, device="cuda"):
             model_id,
             dtype=torch.float16,
             device_map=device,
-            attn_implementation="sdpa",
+            attn_implementation="flash_attention_2",
             trust_remote_code=True,
             low_cpu_mem_usage=True
             )
