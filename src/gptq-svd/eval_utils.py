3514bc1faa24e4047bd1e3f1b17961e64ff32222
David Tweedle <david.aig.tweedle@gmail.com>
2026-01-15 17:54:44 -0500

    updated eval

--- src/gptq-svd/eval_utils.py
+++ src/gptq-svd/eval_utils.py
@@ -19,7 +19,7 @@ def evaluate_perplexity(
         tokenizer,
         dataset: str = "wikitext2",
         device: str = "cuda",
-        batch_size=4,
+        batch_size=16,
         stride=512
         ) -> float:
     """
