e7c55c8d61d91e492a3078fe064396dd69807c58
David Tweedle <david.aig.tweedle@gmail.com>
2026-01-18 22:45:45 -0500

    wrong batch size

--- src/TruncGPTQ/eval_utils.py
+++ src/TruncGPTQ/eval_utils.py
@@ -19,7 +19,7 @@ def evaluate_perplexity(
         tokenizer,
         dataset: str = "wikitext2",
         device: str = "cuda",
-        batch_size=8,
+        batch_size=4,
         stride=512
         ) -> float:
     """
